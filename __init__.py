d99d65b63aaa394619c4e711a36345b23669845c
edunford <eric.dunford@georgetown.edu>
2019-11-26 15:41:08 -0500

    Configure code to generate package

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from .version import __version__
-from .tidysqlite import tidysqlite
-
-__all__ = [
-    'tidysqlite'
-]
---
+++ tidysqlite/__init__.py
@@ -0,0 +1,6 @@
+from tidysqlite.version import __version__
+from tidysqlite.tidysqlite import tidysqlite
+
+__all__ = [
+    'tidysqlite'
+]
